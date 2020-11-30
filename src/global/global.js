4229f4231113fcefad36e789bfb67149646daea2
goodboy3 <zhangzhengbo@hotmail.com>
2020-11-30 19:42:12 +0800

    new white paper

--- src/global/global.js
+++ src/global/global.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-11-10 07:22:35
- * @LastEditTime: 2020-11-27 15:35:52
+ * @LastEditTime: 2020-11-30 15:32:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /coldCDNWeb/src/global/global.js
