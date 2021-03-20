8bceed3e9d074b7b0769d17d577d52314e2e1281
goodboy3 <zhangzhengbo@hotmail.com>
2021-03-20 18:47:53 +0800

    new assets address

--- src/global/global.js
+++ src/global/global.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2020-11-10 07:22:35
- * @LastEditTime: 2021-03-17 12:30:30
+ * @LastEditTime: 2021-03-18 10:02:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /coldCDNWeb/src/global/global.js
  */
 class Global {
-    static apiHost = "https://mesondev.online";
-    static coldCdnApiHost ="https://coldcdndev.online"
+    static apiHost = "https://meson.network";
+    static coldCdnApiHost ="https://coldcdn.com"
 
     //s3 acc url
     static s3BindDomain = Global.coldCdnApiHost+"/api/cdn/hx9216";
--- src/pages/nodeMap/nodeMap.js
+++ src/pages/nodeMap/nodeMap.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-12 13:32:00
- * @LastEditTime: 2021-03-17 18:42:34
+ * @LastEditTime: 2021-03-20 15:44:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /mesonweb/src/pages/nodeMap/nodeMap.js
@@ -21,110 +21,173 @@ export default class NodeMapPage extends React.Component {
     super(props);
 
     const jscode = `
-    var myearth;
-var sprites = [];
+    
+    var script = document.createElement('script');
+document.head.appendChild(script); // insert into the <head></head> tag
 
-window.addEventListener( 'load', function() {
-
-	myearth = new Earth( 'myearth', {
+script.addEventListener('load', ev => { // when the js execute done
+  myearth = new Earth( 'myearth', {
 	
-		location : { lat: 20, lng : 20 },
-	
-		light: 'none',
+    location : { lat: 20, lng : 20 },
 
-		mapImage: '/mapdot/hologram-map.svg',
-		transparent: true,
-		
-		autoRotate : true,
-		autoRotateSpeed: 2.0,
-		autoRotateDelay: 100,
-		autoRotateStart: 2000,			
-		
-	} );
-	
-	
-	myearth.addEventListener( "ready", function() {
+    light: 'none',
 
-		this.startAutoRotate();
-
-		// connections
-		
-		var line = {
-			color : '#009CFF',
-			opacity: 0.35,
-			hairline: true,
-			offset: -0.5
-		};
-
-    let that=this
-
-    var settings = {
-      "url": "/api/v1/common/nodelocation",
-      "method": "GET",
-  };
-   
-    $.ajax(settings).done(function (response) {
-      if (response.status == 0) {
-        // console.log(response.data)
-        connections = response.data
-        console.log(connections)
-
-        for ( var i in connections ) {			
-          line.locations = [ 
-          { lat: connections[i][0], lng: connections[i][1] },
-          { lat: connections[i][2], lng: connections[i][3] } ];
-          that.addLine( line );
-        }
-        
-        
-        
-        // add 8 shine sprites
-       
-        for ( var i=0; i < connections.length; i++ ) {
-          sprites[i] = that.addSprite( {
-            image: '/mapdot/hologram-shine-green2.svg',
-            scale: 0.01,
-            offset: -0.5,
-            opacity: 0.5
-          } );
-          pulse( i );
-        }
-          
-      }
+    mapImage: 'https://assets.meson.network:10443/static/map/hologram/hologram-map.svg',
+    transparent: true,
+  
+    autoRotate : true,
+    autoRotateSpeed: 2.0,
+    autoRotateDelay: 100,
+    autoRotateStart: 2000,			
+  
   });
+
+
+  myearth.addEventListener( "ready", function() {
+
+    this.startAutoRotate();
+
+    // connections
+    
+    var line = {
+      color : '#009CFF',
+      opacity: 0.35,
+      hairline: true,
+      offset: -0.5
+    };
+
+  let that=this
+
+  var settings = {
+    "url": "/api/v1/common/nodelocation",
+    "method": "GET",
+};
+ 
+  $.ajax(settings).done(function (response) {
+    if (response.status == 0) {
+      // console.log(response.data)
+      connections = response.data
+      console.log(connections)
+
+      for ( var i in connections ) {			
+        line.locations = [ 
+        { lat: connections[i][0], lng: connections[i][1] },
+        { lat: connections[i][2], lng: connections[i][3] } ];
+        that.addLine( line );
+      }
+      
+      // add shine sprites
+      for ( var i=0; i < connections.length; i++ ) {
+        sprites[i] = that.addSprite( {
+          image: '/map/hologram-shine-green2.svg',
+          scale: 0.01,
+          offset: -0.5,
+          opacity: 0.5
+        } );
+        pulse( i );
+      }
+        
+    }
+});
+});
+});
+
+script.src = 'https://assets.meson.network:10443/static/map/miniature.earth.core.js'; // start execute a js, can delay
+
+    var myearth;
+    var sprites = [];
+
+    function getRandomInt(min, max) {
+	    min = Math.ceil(min);
+	    max = Math.floor(max);
+	    return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+
+    function pulse( index ) {
+	    var random_location = connections[ index ];
+	    sprites[index].location = { lat: random_location[0] , lng: random_location[1] };
+	
+	    sprites[index].animate( 'scale', 0.8, { duration: 920, complete : function(){
+		    this.animate( 'scale', 0.3, { duration: 920, complete : function(){
+			  setTimeout( function(){ pulse( index ); }, getRandomInt(100, 900) );
+		    }});
+	    }});
+    }
+
+    // locations conntected by lines and places where hologram shines appear
+    var connections = [
+	    [35.4,139.53,	35.4,139.53],
+      [1.2,103.1, 1.2,103.1     ],
+    ];
+
+    window.addEventListener( 'load', function() {
+
+	//     myearth = new Earth( 'myearth', {
+	
+	// 	  location : { lat: 20, lng : 20 },
+	
+	// 	  light: 'none',
+
+	// 	  mapImage: '/mapdot/hologram-map.svg',
+	// 	  transparent: true,
 		
+	// 	  autoRotate : true,
+	// 	  autoRotateSpeed: 2.0,
+	// 	  autoRotateDelay: 100,
+	// 	  autoRotateStart: 2000,			
 		
-		
-		
-	} );
+	//   });
 	
 	
-} );
+	//   myearth.addEventListener( "ready", function() {
 
+	// 	  this.startAutoRotate();
 
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min)) + min;
-}
+	// 	  // connections
+      
+	// 	  var line = {
+	// 		  color : '#009CFF',
+	// 		  opacity: 0.35,
+	// 		  hairline: true,
+	// 		  offset: -0.5
+	// 	  };
 
+  //   let that=this
 
-function pulse( index ) {
-	var random_location = connections[ index ];
-	sprites[index].location = { lat: random_location[0] , lng: random_location[1] };
-	
-	sprites[index].animate( 'scale', 0.8, { duration: 920, complete : function(){
-		this.animate( 'scale', 0.3, { duration: 920, complete : function(){
-			setTimeout( function(){ pulse( index ); }, getRandomInt(100, 900) );
-		} });
-	} });
-}
+  //   var settings = {
+  //     "url": "/api/v1/common/nodelocation",
+  //     "method": "GET",
+  // };
+   
+  //   $.ajax(settings).done(function (response) {
+  //     if (response.status == 0) {
+  //       // console.log(response.data)
+  //       connections = response.data
+  //       console.log(connections)
 
-// locations conntected by lines and places where hologram shines appear
-var connections = [
-	 [35.4,139.53,	35.4,139.53],
-   [1.2,103.1, 1.2,103.1     ],
-];
+  //       for ( var i in connections ) {			
+  //         line.locations = [ 
+  //         { lat: connections[i][0], lng: connections[i][1] },
+  //         { lat: connections[i][2], lng: connections[i][3] } ];
+  //         that.addLine( line );
+  //       }
+        
+  //       // add shine sprites
+  //       for ( var i=0; i < connections.length; i++ ) {
+  //         sprites[i] = that.addSprite( {
+  //           image: '/mapdot/hologram-shine-green2.svg',
+  //           scale: 0.01,
+  //           offset: -0.5,
+  //           opacity: 0.5
+  //         } );
+  //         pulse( i );
+  //       }
+          
+  //     }
+  // });
+	// });
+});
     `;
     new Function(jscode)();
 
@@ -133,7 +196,7 @@ var connections = [
         // { name: "Hangzhou", value: [120.16, 30.29, 24] },
         // { name: "San Francisco", value: [-122.42, 37.77, 10] },
       ],
-      activeNode:[]
+      activeNode: [],
     };
   }
 
@@ -147,6 +210,10 @@ var connections = [
       return;
     }
     let responseData = response.data.data;
+    responseData.sort(function(x,y){
+      return x.id-y.id
+    })
+    
     //console.log(responseData);
 
     //summarize data
@@ -173,7 +240,6 @@ var connections = [
     this.setState({ activeNode: responseData });
   }
 
-  
   renderPoint() {
     return (
       <div>
@@ -238,40 +304,49 @@ var connections = [
             style={{
               position: "absolute",
               backgroundColor: "rgb(0 0 0 / 14%)",
-              marginRight:"5px",
+              marginRight: "5px",
               left: "5px",
               top: "80px",
               maxHeight: "260px",
               overflowY: "scroll",
-              zIndex:"1000000",
-              border:"1px solid #ffffff1a"
+              zIndex: "1000000",
+              border: "1px solid #ffffff1a",
             }}
           >
-            <table border="1" style={{minWidth:"300px",color:"#e0e0e0",borderColor:"#013c6f"}}>
+            <table
+              border="1"
+              style={{
+                minWidth: "300px",
+                color: "#e0e0e0",
+                borderColor: "#013c6f",
+              }}
+            >
               <tbody>
                 <tr>
                   <th>Id</th>
                   <th>Place</th>
                   <th>Bandwidth</th>
                 </tr>
-                {this.state.activeNode.map((value,index,array)=>{
-                  let speed=(value.machine_net_speed*8 / 1000).toFixed(2)
+                {this.state.activeNode.map((value, index, array) => {
+                  let speed = ((value.machine_net_speed * 8) / 1000).toFixed(2);
                   return (
-                    
                     <tr>
-                      <td style={{padding:"0px 5px"}}>{"id-"+value.id}</td>
-                      <td><div style={{maxWidth:"150px",fontSize:"16px"}}>{value.country+" ***  ["+value.city+"]"}</div></td>
-                      <td style={{padding:"0px 10px"}}>{speed} Mb/s</td>
+                      <td style={{ padding: "0px 5px" }}>{"id-" + value.id}</td>
+                      <td>
+                        <div style={{ maxWidth: "150px", fontSize: "16px" }}>
+                          {value.country + " ***  [" + value.city + "]"}
+                        </div>
+                      </td>
+                      <td style={{ padding: "0px 10px" }}>{speed} Mb/s</td>
                     </tr>
-                  )
+                  );
                 })}
-                
               </tbody>
             </table>
           </div>
 
-          <div id="myearth" style={{height:"500px"}}>
-            <div id="glow" style={{height:"500px",width:"400px"}}></div>
+          <div id="myearth" style={{ height: "500px" }}>
+            <div id="glow" style={{ height: "500px", width: "400px" }}></div>
           </div>
         </div>
       </>
